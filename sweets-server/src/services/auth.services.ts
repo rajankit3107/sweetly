856a7cd82018f18b75763469ac88d7910a032991
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-18 23:53:59 +0530

    feat : added login services

--- sweets-server/src/services/auth.services.ts
+++ sweets-server/src/services/auth.services.ts
@@ -2,6 +2,7 @@ import { User } from "../models/user.model";
 import { ApiError } from "../utils/apiError";
 import { getEnv } from "../utils/getEnv";
 import bcrypt from "bcryptjs";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 const JWT_SECRET = getEnv("JWT_SECRET");
 const JWT_EXPIRY = getEnv("JWT_EXPIRY");
@@ -22,4 +23,24 @@ export async function register(
   });
 
   return { id: user._id, username: user.username, role: user.role };
+}
+
+export async function login(username: string, password: string) {
+  const user = await User.findOne({ username });
+
+  if (!user) throw ApiError.unauthorized(`Invalid credentials`);
+
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) throw ApiError.unauthorized(`Invalid credentials`);
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+      role: user.role,
+    },
+    JWT_SECRET,
+    { expiresIn: JWT_EXPIRY } as SignOptions
+  );
 }
