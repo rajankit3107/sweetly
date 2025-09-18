0fc576c9c74b3a75e220c8a0b6b18489969c20c2
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-19 01:44:01 +0530

    update : added validation in routes

--- sweets-server/src/routes/authRoutes.ts
+++ sweets-server/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller";
+import { validateBody } from "../middlewares/validation.middleware";
+import { loginSchema, registerSchema } from "../validators/auth.validator";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateBody(registerSchema), register);
+router.post("/login", validateBody(loginSchema), login);
 
 export default Router;
