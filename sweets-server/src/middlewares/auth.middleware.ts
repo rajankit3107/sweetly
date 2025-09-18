359e6a6d447b1e0b61e1f03d608f6256dc94b919
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-19 00:22:28 +0530

    feat(auth): add JWT-based authentication middleware

---
+++ sweets-server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { getEnv } from "../utils/getEnv";
+import { ApiError } from "../utils/apiError";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = getEnv("JWT_SECRET");
+
+export interface AuthRequest extends Request {
+  user?: { id: string; username: string; role: string };
+}
+
+export function requireAuth(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith(`Bearer `)) {
+    return next(ApiError.unauthorized(`Unauthorized`));
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET) as any;
+    req.user = {
+      id: payload.id,
+      username: payload.username,
+      role: payload.role,
+    };
+    next();
+  } catch (error) {
+    return next(ApiError.unauthorized(`Invalid token`));
+  }
+}
