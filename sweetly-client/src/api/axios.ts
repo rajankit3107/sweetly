9b616fa5bac19be78ccd53f151b6c7194c7389f3
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-09-20 19:27:05 +0530

    fix : added checks for env variables to propogate.

--- sweetly-client/src/api/axios.ts
+++ sweetly-client/src/api/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+if (!baseURL) {
+  throw new Error("VITE_API_BASE_URL is not set");
+}
 
 const api = axios.create({
   baseURL,
